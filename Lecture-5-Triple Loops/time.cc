d504c773edf35746e15ff0bead19029b42698ec5
Yaniv Gabay <yanivga@std.hac.ac.il>
2024-01-20 15:20:14 +0200

    added boxes example, and variable name importance

--- Lecture-5-Triple Loops/time.cc
+++ Lecture-5-Triple Loops/time.cc
@@ -1,27 +1,37 @@
+///////------//lecture 5 - triple loops - time example//------///////
+
+/*in this example, we will show a usage of triple nested loops
+   to display all the possible times in a day in HH:MM:SS format.
+   this is using for loops, but you can make the same print using while loops.
+   */
+
+//include section
 #include <iostream>
 #include <iomanip> // For std::setw
 
-int main() {
-    const int hoursLimit = 24;
-    const int minutesLimit = 60;
-    const int secondsLimit = 60;
+//main section
+int main()
+{
+    // variables section
+    int hoursLimit = 24;
+    int minutesLimit = 60;
+    int secondsLimit = 60;
 
-    /*this example, we will show a usage of triple nested loops
-    to display all the possible times in a day in HH:MM:SS format.
-    this is using for loops, but you can make the same print using while loops.
-    */
+    //triple nested loop to print all the possible times in a day
 
-
-   // can run from 0 to 23
-    for (int hour = 0; hour < hoursLimit; hour++) { 
+    // can run from 0 to 23
+    for (int hour = 0; hour < hoursLimit; hour++)
+    {
         // can run from 0 to 59 for each hour
-        for (int minute = 0; minute < minutesLimit; minute++) {
+        for (int minute = 0; minute < minutesLimit; minute++)
+        {
             // can run from 0 to 59 for each minute
-            for (int second = 0; second < secondsLimit; second++) {
-                
-                std::cout  << hour << ":"
-                           << minute << ":"
-                           << second << std::endl;
+            for (int second = 0; second < secondsLimit; second++)
+            {
+                // prints: "hour:minute:second" endl
+                std::cout << hour << ":"
+                          << minute << ":"
+                          << second << std::endl;
             }
         }
     }
@@ -29,9 +39,9 @@ int main() {
     the rightest numbers, "change" the most, and we can see that its almost looks
     like a premant rolling. the leftest numbers, "change" the least,
      and we can barley see them change    */
-     // the for loops for hours, - least amount of cycles
-     // the for loops for minutes, - medium amount of cycles
-     // the for loops for seconds, - most amount of cycles by far.
+    // the for loops for hours, - least amount of cycles
+    // the for loops for minutes, - medium amount of cycles
+    // the for loops for seconds, - most amount of cycles by far.
 
     return 0;
 }
