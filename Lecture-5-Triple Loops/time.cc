2048b14d9d0f29bbcfcb089b7b2d3db8e7acdf4e
Yaniv Gabay <yanivga@std.hac.ac.il>
2024-01-20 14:52:09 +0200

    first lecture 5 code example,

---
+++ Lecture-5-Triple Loops/time.cc
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <iomanip> // For std::setw
+
+int main() {
+    const int hoursLimit = 24;
+    const int minutesLimit = 60;
+    const int secondsLimit = 60;
+
+    /*this example, we will show a usage of triple nested loops
+    to display all the possible times in a day in HH:MM:SS format.
+    this is using for loops, but you can make the same print using while loops.
+    */
+
+
+   // can run from 0 to 23
+    for (int hour = 0; hour < hoursLimit; hour++) { 
+        // can run from 0 to 59 for each hour
+        for (int minute = 0; minute < minutesLimit; minute++) {
+            // can run from 0 to 59 for each minute
+            for (int second = 0; second < secondsLimit; second++) {
+                
+                std::cout  << hour << ":"
+                           << minute << ":"
+                           << second << std::endl;
+            }
+        }
+    }
+    /*what we can learn from the output of this program is that
+    the rightest numbers, "change" the most, and we can see that its almost looks
+    like a premant rolling. the leftest numbers, "change" the least,
+     and we can barley see them change    */
+     // the for loops for hours, - least amount of cycles
+     // the for loops for minutes, - medium amount of cycles
+     // the for loops for seconds, - most amount of cycles by far.
+
+    return 0;
+}
