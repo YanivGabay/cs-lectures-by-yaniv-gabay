57ab26300a60f85218b47ed9614cb0add5edf07a
Yaniv Gabay <yanivga@std.hac.ac.il>
2024-02-11 22:32:11 +0200

    fixed

--- Lecture-8-/initilizingArrays.cc
+++ Lecture-8-/initilizingArrays.cc
@@ -19,7 +19,7 @@ using ::std::endl;
 
 
 // prototypes section
-
+void printArray(int arr[], int size);
 // using section
 using ::std::cin;
 using ::std::cout;
@@ -32,8 +32,19 @@ const int NUM_OF_COURSES = 5;
 int main()
 {
     int grades[IDS][NUM_OF_COURSES];
+    cout << "before memeset with grabage:" << endl;
+    for (int row = 0; row < IDS; row++)
+    {
+        printArray(grades[row], NUM_OF_COURSES);
+    }
 
     memset(grades, 0, sizeof(grades)) ;
+    cout << "after memeset with grabage:" << endl;
+    for (int row = 0; row < IDS; row++)
+    {
+        printArray(grades[row], NUM_OF_COURSES);
+    }
+    
     // give it grades array
     // give the value to fill
     // size of (how many bytes to fill)
@@ -43,4 +54,11 @@ int main()
 }
 
 //-------
-void print
+void printArray(int arr[], int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
