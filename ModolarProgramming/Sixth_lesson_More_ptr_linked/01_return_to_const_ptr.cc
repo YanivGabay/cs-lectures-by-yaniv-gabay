601d2dbd05d74e811ccda38b750c20fb4df3f13f
Yaniv Gabay <yanivga@std.hac.ac.il>
2024-05-13 04:21:56 +0300

    link to const ptrs explain

---
+++ ModolarProgramming/Sixth_lesson_More_ptr_linked/01_return_to_const_ptr.cc
@@ -0,0 +1,5 @@
+https://www.geeksforgeeks.org/difference-between-const-int-const-int-const-and-int-const/
+
+
+
+https://www.geeksforgeeks.org/difference-between-const-int-const-int-const-and-int-const/
