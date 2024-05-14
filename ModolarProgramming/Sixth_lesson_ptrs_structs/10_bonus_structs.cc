81bc53f65d0f28f10275a8a45e7db086645a9e5b
Yaniv Gabay <yanivga@std.hac.ac.il>
2024-05-15 00:02:41 +0300

    updated the last example

--- ModolarProgramming/Sixth_lesson_ptrs_structs/10_bonus_structs.cc
+++ ModolarProgramming/Sixth_lesson_ptrs_structs/10_bonus_structs.cc
@@ -7,65 +7,80 @@
  * Date: 2024-05-13
  *
  * Overview:
- * This example demonstrates the basic implementation of a singly linked list
- * in C++. The linked list allows adding elements and printing the list.
+ * This example demonstrates the basic implementation of a custom String struct
+ * in C++. The struct allows creating strings, printing them, and getting their length.
  *******************************************************************/
 
 // ---------- Include Section ----------
 #include <iostream>
 #include <cstring>
+
 // ---------- Using Section ----------
 using std::cin;
 using std::cout;
 using std::endl;
-
 
 const int MAX = 50;
 
 // ---------- Structs ----------
 struct String {
     char _str[MAX];
-    int _length;
+    size_t _length;
 
-    void print() {
-        cout << _str << endl;
-    }
-    int getLength() {
-        return _length;
-    }
-    String() { // constructor // you will learn about constructors in the next year
-        _length = 5;
+    // Default constructor
+    String() {
+        _length = 0;
         _str[0] = '\0';
     }
-    //can create a constructor with parameters
+
+    // Constructor with parameters
     String(const char* str) {
         strcpy(_str, str);
         _length = strlen(str);
     }
-};
 
+    // Function to print the string
+    void print() const {
+        cout << _str << endl;
+    }
+
+    // Function to get the length of the string
+    size_t getLength() const {
+        return _length;
+    }
+
+    // Function to set the string value
+    void setString(const char* str) {
+        strcpy(_str, str);
+        _length = strlen(str);
+    }
+};
 
 // ---------- Function Prototypes ----------
 
 
 // ---------- Main Function ----------
 int main() {
-    
+    // Example using the default constructor
     String str;
-    cout << "length: " << str._length << endl; // 0
-    strcpy(str._str, "Hello");
-    str._length = 5;
-    str.print();
+    cout << "Default constructor - length: " << str.getLength() << endl; // 0
+    str.setString("Hello");
+    cout << "After setting string: ";
+    str.print(); // Hello
 
+    //could be int, but strlen returned size_t , so i prefer to use size_t
+    size_t length = str.getLength();
+    cout << "Length: " << length << endl; // 5
 
-    int length = str.getLength();
-    cout << "Length: " << length << endl;
-
-    //example of a constructor with parameters
+    // Example using the constructor with parameters
     String str2("World");
-    str2.print();
+    cout << "Constructor with parameters: ";
+    str2.print(); // World
+
+    cout << "Length of str2: " << str2.getLength() << endl; // 5
+
     return 0;
 }
 
 // ---------- Functions ----------
-
+// Add any additional functions if needed
