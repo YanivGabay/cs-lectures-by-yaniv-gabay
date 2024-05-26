be146df537b19bb0cb018e03a7a631ac10bdc835
Yaniv Gabay <yanivga@std.hac.ac.il>
2024-05-27 00:57:05 +0300

    three questions about linked lists

--- ModolarProgramming/Seventh_lesson_linked_lists/05_given_linked_list.cc
+++ ModolarProgramming/Seventh_lesson_linked_lists/05_given_linked_list.cc
@@ -57,11 +57,13 @@ void printList(const Node* head,Ptr_Type type);
 void deleteList(Node*& head);
 void enterDataLoop(Node*& head,const int* numbers, int arrSize);
 void insertNodeAtStart(Node*& head, Node* newNode);
-
+int findMax(const Node* head);
+void reverseList(Node** head_ref);
+bool findTwoNegatives(const Node* head);
 // ---------- Main Function ----------
 int main() {
     Node* head = nullptr; // Initialize the head of the list to nullptr
-    const int numbers[] = { 10, 20, 225, 30 ,-5,8,53,6,111,-9,0,1,1245153,1,535435, 4535,1,-1,2,4,5,4 };
+    const int numbers[] = { 10, 20, 225, 30 ,-5,-8,53,6,111,-9,0,1,1245153,1,535435, 4535,1,-1,2,4,5,4 };
     cout << "enterting data recursion\n";
     int arrSize = sizeof(numbers) / sizeof(numbers[0]);
     enterDataLoop(head, numbers,arrSize);
@@ -69,11 +71,24 @@ int main() {
     printList(head,HEAD);
     
     //now lets ask ourselves questions:
-    //1. how can we print the list from the end to the start?
-    //2. find the max value in the list
-    //3. search for two negative numbers in a row
+   
+    //1. find the max value in the list
+    //2. search for two negative numbers in a row
+    //3. how can we print the list from the end to the start?
 
-    //1:
+    //question 1:
+    cout << "Max value in the list: " << findMax(head) << endl;
+
+    //question 2:
+  
+    if (findTwoNegatives(head)) {
+        cout << "Two negative numbers in a row were found" << endl;
+    } else {
+        cout << "Two negative numbers in a row were not found" << endl;
+    }
+
+    //question 3:
+    
     cout << "Linked List from the end: ";
     reverseList(&head);
     printList(head,HEAD);
@@ -88,8 +103,37 @@ int main() {
 }
 
 // ---------- Functions ----------
+bool findTwoNegatives(const Node* head) {
+    const Node* current = head;
+    while (current != nullptr) {
+        if (current->data < 0 && current->next != nullptr && current->next->data < 0) {
+            cout << "Two negative numbers in a row: " << current->data << " " << current->next->data << endl;
+            return true;
+        }
+        current = current->next;
+    }
+    return false;
+}
 
-//question 1:
+int findMax(const Node* head) {
+    //VERY IMPROTANT
+    //functions that get a pointer to a struct, should always check if the pointer is nullptr
+    if (head == nullptr) {
+        return -1;
+    }
+    int max = head->data;
+    const Node* current = head;
+    while (current != nullptr) {
+        if (current->data > max) {
+            max = current->data;
+        }
+        current = current->next;
+    }
+    return max;
+}
+
+
+//question 3:
 //we will just reverse the list and print it
 // 3 ptrs: previous, current, next
 void reverseList(Node** head_ref) {
