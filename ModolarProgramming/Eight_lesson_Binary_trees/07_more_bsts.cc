f46d8f0c40963613a3daf579098c31b1ce03c370
yaniv <yaniv@DESKTOP-IBQLOSB>
2024-06-16 07:57:03 +0300

    another  example of searching for subtress

---
+++ ModolarProgramming/Eight_lesson_Binary_trees/07_more_bsts.cc
@@ -0,0 +1,143 @@
+/********************************************************************
+ * Course: Modular Programming in C++
+ * Lecture: 
+ * File: .cc
+ *
+ * Author: Yaniv Gabay
+ * Date: 2024-6-3
+ *
+ * Overview:
+
+ *******************************************************************/
+
+
+// ---------- Include Section ----------
+#include <iostream>
+
+// ---------- Using Section ----------
+using std::cin;
+using std::cout;
+using std::endl;
+
+// ---------- Structs ----------
+struct Node {
+    int data;
+    Node* right;
+    Node* left;
+};
+
+// ---------- Function Prototypes ----------
+void insert_into_tree(Node*& root, int data);
+void print_tree(const Node* const root);
+
+
+int smallest(const Node* const root);
+int largest(const Node* const root);
+int smallest_biggest_sum(const Node* const root);
+int sum_of_leafs(const Node* const root,int &sum);
+// ---------- Main Function ----------
+int main() {
+    Node* root = nullptr; // Initialize the head of the list to nullptr
+    insert_into_tree(root, 10);
+    insert_into_tree(root, 20);
+    insert_into_tree(root, 30);
+    insert_into_tree(root, 5);
+    insert_into_tree(root, -15);
+
+
+
+
+    //TO BE FIXED
+    print_tree(root);
+    cout << "smallest and largest sum is: " << smallest_biggest_sum(root) << endl;
+    int sum = 0;
+    sum = sum_of_leafs(root,sum);
+    cout << "sum of leafs is: " << sum;
+
+    return 0;
+}
+
+// ---------- Functions ----------
+int sum_of_leafs(const Node* const root,int&sum)
+{
+    cout << "inside sum_of_leafs" << endl;
+    if(root == nullptr)
+    {
+        cout << "root equal null" << endl;
+          return 0;
+    }
+   
+    
+    if(root->left==nullptr && root->right==nullptr)
+    {
+        cout << "found a leaf" << endl;
+        cout << "returning: " << root->data;
+            return root->data;
+
+    }
+
+    sum += sum_of_leafs(root->left,sum);
+    sum += sum_of_leafs(root->right,sum);
+
+  
+}
+
+int smallest_biggest_sum(const Node* const root)
+{
+    return smallest(root)+largest(root);
+}
+int smallest(const Node* const root)
+{
+    //should return -15 +30  
+    if(root->left == nullptr)
+    return root->data;
+
+    smallest(root->left);
+
+
+}
+int largest(const Node* const root)
+{
+    if(root->right == nullptr)
+    return root->data;
+
+    largest(root->right);
+}
+void print_tree(const Node* const root)
+{
+    if(root == nullptr)
+    return;
+
+    print_tree(root->left);
+    cout << root->data << " ";
+    print_tree(root->right);
+
+    return;
+}
+
+void delete_tree(Node*& root) {
+    if (root == nullptr) {
+        return;
+    }
+    delete_tree(root->left);
+    delete_tree(root->right);
+    delete root;
+    //some like this, some not, but it is a good practice in my opinion
+    root = nullptr;
+}
+
+
+
+void insert_into_tree(Node*& root, int data) {
+    if (root == nullptr) {
+        root = new Node;
+        //check allocation
+        root->data = data;
+        root->left = nullptr;
+        root->right = nullptr;
+    } else if (data < root->data) {
+        insert_into_tree(root->left, data);
+    } else {
+        insert_into_tree(root->right, data);
+    }
+}
---
+++ ModolarProgramming/Ninth_lesson_bst/4_subtree.cc
@@ -0,0 +1,93 @@
+/********************************************************************
+ * Course: Modular Programming in C++
+ * Lecture: 
+ * File: .cc
+ *
+ * Author: Yaniv Gabay
+ * Date: 2024-6-3
+ *
+ * Overview:
+
+ *******************************************************************/
+
+// ---------- Include Section ----------
+#include <iostream>
+
+// ---------- Using Section ----------
+using std::cin;
+using std::cout;
+using std::endl;
+
+// ---------- Structs ----------
+struct Node {
+    int data;
+    Node* right;
+    Node* left;
+};
+
+// ---------- Function Prototypes ----------
+void insert_into_tree(Node*& root, int data);
+void display(const Node* root);
+
+
+
+int subTreesNegativeOnly(Node* root, int& count) ;
+int subtreeSum(Node* root) ;
+// ---------- Main Function ----------
+int main() {
+    Node* root = nullptr; // Initialize the head of the list to nullptr
+    insert_into_tree(root, 10);
+    insert_into_tree(root, 20);
+    insert_into_tree(root, 30);
+    insert_into_tree(root, -4);
+    insert_into_tree(root, -14);
+
+    display(root);
+    int count = 0;
+    subTreesNegativeOnly(root, count);
+    std::cout << "Number of subtrees with all negatives "  << ": " << count << std::endl;
+    return 0;
+}
+
+// ---------- Functions ----------
+bool allNegatives(Node* root) {
+    if (root == nullptr)
+     return false;
+    if(root->data < 0)
+    return true;
+    return allNegatives(root->left) && allNegatives(root->right);
+}
+
+// Function to count how many subtrees meet the specified sum condition
+int subTreesNegativeOnly(Node* root, int& count) {
+    if (root == nullptr) return 0;
+    bool result  = allNegatives(root);
+    if (result) {
+        count++;
+    }
+    subTreesNegativeOnly(root->left,  count);
+    subTreesNegativeOnly(root->right,  count);
+    return count;
+}
+
+void display(const Node* root) {
+    if (root == nullptr) {
+        return;
+    }
+    display(root->left);
+    cout << root->data << " ";
+    display(root->right);
+}
+void insert_into_tree(Node*& root, int data) {
+    if (root == nullptr) {
+        root = new Node;
+        //check allocation
+        root->data = data;
+        root->left = nullptr;
+        root->right = nullptr;
+    } else if (data < root->data) {
+        insert_into_tree(root->left, data);
+    } else {
+        insert_into_tree(root->right, data);
+    }
+}
