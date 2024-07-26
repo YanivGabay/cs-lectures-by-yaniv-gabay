25da287cd7fa3ef803800686bb04acad3e0e1511
Yaniv Gabay <yanivga@std.hac.ac.il>
2024-07-26 15:00:32 +0300

    adding a new lesson, new test questions for the students

---
+++ ModolarProgramming/lesson-14-more-tests/2022A-1-avg-salary-diff.cc
@@ -0,0 +1,46 @@
+/********************************************************************
+ * Course: Modolar Programming
+ * Lecture: 13 - tests
+ * File: 2022A-1-avg-salary-diff.cc
+ * 
+ * Author: Yaniv Gabay
+ * Date: 2024-07-26
+ * 
+ * Overview:
+ * 
+ * 
+ * Notes:
+ * Any additional notes related to this file.
+ *******************************************************************/
+
+// ---------- Include Section ----------
+#include <iostream>
+// Add more #include directives as needed
+
+// ---------- Using Section ----------
+using std::cout;
+using std::cin;
+using std::endl;
+
+// ---------- Constants ----------
+
+
+// ---------- Structs ----------
+
+struct Node {
+    int _salary;
+    struct Node *_left, *_right;
+
+};
+// ---------- Function Prototypes ----------
+
+
+// ---------- Main Function ----------
+int main() {
+    // Main function code
+    cout << "Hello, World!" << endl;
+
+    return 0;
+}
+
+// ---------- Functions ----------
---
+++ ModolarProgramming/lesson-14-more-tests/general-notes.md
@@ -0,0 +1,10 @@
+## General notes before the Test
+
+
+### Tips
+
+- **Read the question carefully**. Make sure you understand what is being asked. If you don't understand, ask the teacher.
+- **Write like they like to write**. If the question asks for a function, write a function, with the correct signature and return type.
+- also, think about the syntax Yoram likes to see. For example, he likes to see `int* arr` instead of `int *arr` , or `_memeber` instead of `member`.
+- **Write notes to explain your mindset**. If you are not sure about the solution, write down your thoughts. It will help you to get partial credit, i wanted to do this, and than sort this, and extract this using this function, but i didn't have time to finish it etc.
+- 
