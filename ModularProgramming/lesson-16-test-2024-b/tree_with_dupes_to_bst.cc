eee7e63939325ddd7e08d6c82056de94802d4f9b
Yaniv Gabay <yanivga@edu.hac.ac.il>
2024-09-18 06:51:25 +0300

    fixed the solution to the binary tree question

--- ModularProgramming/lesson-16-test-2024-b/tree_with_dupes_to_bst.cc
+++ ModularProgramming/lesson-16-test-2024-b/tree_with_dupes_to_bst.cc
@@ -28,11 +28,15 @@ int const MAX_SIZE = 1000;
 // ---------- Structs ----------
 struct Node {
     int data;
-    Node* left;
-    Node* right;
+    Node* left = nullptr;
+    Node* right = nullptr;
 };
 
 // ---------- Function Prototypes ----------
+void print_tree(Node* root);
+Node* build_new_tree(Node* root);
+void count_values(Node* root, int counter_array[], int &highest_value);
+Node* insert(Node*& root, int value);
 
 
 // ---------- Main Function ----------
@@ -42,12 +46,46 @@ int main() {
     //we need to convert it to a bst without duplicates
     //with building a new tree
     Node* old_root;
+    //lets do an example tree with multiply values
+    old_root = new Node;
+    old_root->data = 5;
+    old_root->left = new Node;
+    old_root->left->data = 5;
+    old_root->left->left = nullptr;
+    old_root->left->right = nullptr;
+    old_root->right = new Node;
+    old_root->right->data = 15;
+    old_root->right->left = new Node;
+    old_root->right->left->data = 15;
+    old_root->right->left->left = nullptr;
+    old_root->right->left->right = new Node;
+    old_root->right->left->right->data = 150;
+    
+    print_tree(old_root);
     Node* new_tree = build_new_tree(old_root);
+    cout << "New tree: ";
+    if (new_tree == nullptr) {
+        cout << "Empty tree";
+    }
+    else {
+        print_tree(new_tree);
+    }
+
 
   
 }
 
 // ---------- Functions ----------
+void print_tree(Node* root)
+{
+    if (root == nullptr)
+    {
+        return;
+    }
+    print_tree(root->left);
+    cout << root->data << " ";
+    print_tree(root->right);
+}
 Node* build_new_tree(Node* root) {
 
     int counter_array[MAX_SIZE] = {0};
@@ -56,7 +94,7 @@ Node* build_new_tree(Node* root) {
 
     Node* new_root = nullptr;
     for (int i = 0; i <= highest_value; i++) {
-        if (counter_array[i] != 1) {
+        if (counter_array[i] == 1) {
             new_root = insert(new_root, i);
         }
     }
@@ -79,6 +117,17 @@ Node* insert(Node*& root,int value)
 {
     if (root == nullptr)
     {
-
+        Node* root = new Node;
+        root->data = value;
+        root->left = nullptr;
+        root->right = nullptr;
+    }
+    else if (value <= root->data)
+    {
+        root->left = insert(root->left, value);
+    }
+    else
+    {
+        root->right = insert(root->right, value);
     }
 }
